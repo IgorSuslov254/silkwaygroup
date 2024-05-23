a99a4f36c8737a1a2556f8773177414e35d1924d
Igor Suslov <i.suslov@swiftx.ru>
2024-05-23 17:23:50 +0300

    sw_services module v3 - catalog done

--- catalog/language/en-gb/extension/module/sw_benefit.php
+++ catalog/language/en-gb/extension/module/sw_benefit.php
@@ -5,6 +5,7 @@ $_['heading_title'] = 'BENEFIT';
 
 // Common
 $_['description'] = 'You can earn money on these products';
+$_['button'] = 'Leave a request for a selection';
 
 // Error
 $_['get_error'] = "Error loading module data: \"{$_['heading_title']}\" ";
