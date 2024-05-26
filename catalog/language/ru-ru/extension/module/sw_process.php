a343093e66a408b8b5624f8d165d6c6f83619ebc
IgorSuslov <igirsuslov@gmail.com>
2024-05-26 22:40:22 +0300

    sw_process v3 - lang done

--- catalog/language/ru-ru/extension/module/sw_process.php
+++ catalog/language/ru-ru/extension/module/sw_process.php
@@ -6,6 +6,46 @@ $_['heading_title'] = 'ВЫГОДА';
 // Common
 $_['description'] = 'На этих товарах вы можете заработать';
 $_['button'] = 'Оставьте заявку на подборку';
+$_['h2'] = 'ПРОЦЕСС';
+$_['h3'] = 'Как происходит работа с нами?';
+$_['advantages'] = [
+    [
+        'h4' => '10+',
+        'p' => 'Лет на рынке',
+        'xs' => '6',
+        'md' => '3',
+        'lg' => '2'
+    ],
+    [
+        'h4' => '>50000',
+        'p' => 'Успешных доставок',
+        'xs' => '6',
+        'md' => '3',
+        'lg' => '2'
+    ],
+    [
+        'h4' => '',
+        'p' => 'Для вашего удобства мы сняли короткое видео, в котором рассказали о всех особенностях и этапах нашей работы',
+        'xs' => '12',
+        'md' => '6',
+        'lg' => '8'
+    ]
+];
+$_['instructions'] = [
+    [
+        'h4' => 'Всего 5 шагов',
+        'p' => 'Придется сделать всего пять простых шагов и товар из Китая окажется у вас',
+        'button' => '',
+        'class' => ''
+    ],
+    [
+        'h4' => '',
+        'p' => 'Вы можете связаться с нами онлайн в Telegram и оформить заказ',
+        'button' => 'Оформить заказ',
+        'class' => 'no-card'
+    ]
+];
+$_['you_tube'] = '<iframe width="100%" height="100%" src="https://www.youtube.com/embed/xwCzMUMwynw?si=qkxcOU-3WCtbxpqw" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>';
 
 // Error
 $_['get_error'] = "Ошибка загрузи данных модуля: \"{$_['heading_title']}\" ";
