c454810dfe2043fcf20a50a0cf3bcf416f567446
Igor Suslov <i.suslov@swiftx.ru>
2024-05-30 18:36:41 +0300

    sw_calculation v6 - catalog view process

--- catalog/language/ru-ru/extension/module/sw_calculation.php
+++ catalog/language/ru-ru/extension/module/sw_calculation.php
@@ -6,8 +6,11 @@ $_['heading_title'] = 'Просчет';
 // Common
 $_['description'] = 'Заполните форму ниже и мы рассчитаем стоимость доставки';
 $_['button'] = 'Отправить';
+$_['button_loading_text'] = 'Отправка...';
 
 // Error
 $_['get_error'] = "Ошибка загрузи данных модуля: \"{$_['heading_title']}\" ";
+$_['button_success'] = 'Мы вам скоро позвоним';
 
 // Success
+$_['button_error'] = 'Произошла ошибка';
