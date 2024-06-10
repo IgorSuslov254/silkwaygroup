a9c5dd63a99f5d27bda843b52f18641644f6ff66
Igor Suslov <i.suslov@swiftx.ru>
2024-06-10 17:09:02 +0300

    sw_calc v5 - catalog done

--- catalog/language/en-gb/extension/module/sw_calc.php
+++ catalog/language/en-gb/extension/module/sw_calc.php
@@ -1,1 +1,59 @@
 <?php
+
+$_['text_banner'] = 'If you need an approximate shipping cost,<br>You can calculate it yourself on the calculator.';
+$_['text_button_banner'] = 'Calculate the delivery';
+$_['form'] = [
+    'inputs' => [
+        [
+            'name' => 'width',
+            'type' => 'number',
+            'placeholder' => 'width',
+            'min' => '1',
+            'max' => '5000'
+        ],
+        [
+            'name' => 'length',
+            'type' => 'number',
+            'placeholder' => 'length',
+            'min' => '1',
+            'max' => '5000'
+        ],
+        [
+            'name' => 'depth',
+            'type' => 'number',
+            'placeholder' => 'depth',
+            'min' => '1',
+            'max' => '5000'
+        ],
+        [
+            'name' => 'weight',
+            'type' => 'number',
+            'placeholder' => 'weight',
+            'min' => '1',
+            'max' => '5000'
+        ]
+    ],
+    'selects' => [
+        'cloth_type' => [
+            'name' => 'cloth_type',
+            'default_value' => 'cloth type',
+        ],
+        'route' => [
+            'name' => 'route',
+            'default_value' => 'route',
+        ]
+    ],
+    'p' => [
+        'text_load' => 'The calculation is being made...',
+        'text_answer' => 'Approximate shipping cost: ',
+        'text_error' => 'Calculation error'
+    ],
+    'buttons' => [
+        'submit' => [
+            'text' => 'Calculate'
+        ],
+        'close' => [
+            'text' => 'To close'
+        ]
+    ]
+];
--- catalog/language/ru-ru/extension/module/sw_calc.php
+++ catalog/language/ru-ru/extension/module/sw_calc.php
@@ -1,1 +1,59 @@
 <?php
+
+$_['text_banner'] = 'Если вам нужна примерная стоимость доставки,<br>Вы можете самостоятельно рассчитать её на калькуляторе.';
+$_['text_button_banner'] = 'Рассчитать доставку';
+$_['form'] = [
+    'inputs' => [
+        [
+            'name' => 'width',
+            'type' => 'number',
+            'placeholder' => 'Ширина коробки',
+            'min' => '1',
+            'max' => '5000'
+        ],
+        [
+            'name' => 'length',
+            'type' => 'number',
+            'placeholder' => 'Длина коробки',
+            'min' => '1',
+            'max' => '5000'
+        ],
+        [
+            'name' => 'depth',
+            'type' => 'number',
+            'placeholder' => 'Глубина коробки',
+            'min' => '1',
+            'max' => '5000'
+        ],
+        [
+            'name' => 'weight',
+            'type' => 'number',
+            'placeholder' => 'Вес коробки',
+            'min' => '1',
+            'max' => '5000'
+        ]
+    ],
+    'selects' => [
+        'cloth_type' => [
+            'name' => 'cloth_type',
+            'default_value' => 'Тип груза',
+        ],
+        'route' => [
+            'name' => 'route',
+            'default_value' => 'Маршрут',
+        ]
+    ],
+    'p' => [
+        'text_load' => 'Производится рассчёт...',
+        'text_answer' => 'Приблизительная стоимость доставки: ',
+        'text_error' => 'Ошибка рассчёта'
+    ],
+    'buttons' => [
+        'submit' => [
+            'text' => 'Рассчитать'
+        ],
+        'close' => [
+            'text' => 'Закрыть'
+        ]
+    ]
+];
